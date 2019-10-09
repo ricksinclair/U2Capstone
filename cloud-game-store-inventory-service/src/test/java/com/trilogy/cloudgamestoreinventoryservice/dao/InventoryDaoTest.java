fffa99bf6f82776e5b3d90ce51da287f2209cf7f
Abraham Leal <aleal629@gmail.com>
2019-10-09 14:35:15 -0400

    Inventory Service Layer

---
+++ cloud-game-store-inventory-service/src/test/java/com/trilogy/cloudgamestoreinventoryservice/dao/InventoryDaoTest.java
@@ -0,0 +1,20 @@
+package com.trilogy.cloudgamestoreinventoryservice.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class InventoryDaoTest {
+
+    @Test
+    public void addInventory() {
+    }
+
+    @Test
+    public void getAllInventories() {
+    }
+
+    @Test
+    public void deleteInventory() {
+    }
+}
