d47870c6c6c550d964093bb8a4bda372f64d9600
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-10-09 14:58:17 -0400

    Update InventoryDao.java

--- cloud-game-store-inventory-service/src/main/java/com/trilogy/cloudgamestoreinventoryservice/dao/InventoryDao.java
+++ cloud-game-store-inventory-service/src/main/java/com/trilogy/cloudgamestoreinventoryservice/dao/InventoryDao.java
@@ -1,4 +1,18 @@
 package com.trilogy.cloudgamestoreinventoryservice.dao;
 
+import com.trilogy.cloudgamestoreinventoryservice.model.Inventory;
+
+import java.util.List;
+
 public interface InventoryDao {
+
+    Inventory addInventory(Inventory inventory);
+
+    Inventory getInventory(int inventoryId);
+
+    List<Inventory> getAllInventories();
+
+    void updateInventory(Inventory inventory);
+
+    void deleteInventory(int inventoryId);
 }
