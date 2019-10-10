b4b4b1ecdc9c158a353e9d639b2f61ff16a88822
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-10-10 10:20:41 -0400

    added jenkins stuff

---
+++ cloud-game-store-customer-service/src/test/java/com/trilogy/cloudgamestorecustomerservice/controller/CustomerControllerTest.java
@@ -0,0 +1,14 @@
+package com.trilogy.cloudgamestorecustomerservice.controller;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+
+
+}
