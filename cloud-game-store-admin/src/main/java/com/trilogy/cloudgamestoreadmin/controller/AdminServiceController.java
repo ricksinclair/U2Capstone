1e1a3817226b2b352ef05dc7441794b27cfd398c
Ulric Todman <rick.sinclair.nyc@gmail.com>
2019-10-14 13:22:05 -0400

    fixed previous commit with comment

--- cloud-game-store-admin/src/main/java/com/trilogy/cloudgamestoreadmin/controller/AdminServiceController.java
+++ cloud-game-store-admin/src/main/java/com/trilogy/cloudgamestoreadmin/controller/AdminServiceController.java
@@ -19,57 +19,55 @@ public class AdminServiceController {
 
     @Autowired
     AdminService service;
+
     //CUSTOMER ENDPOINTS
     @RequestMapping(value = "/customers", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public Customer addCustomer(@Valid @RequestBody Customer customer){
+    public Customer addCustomer(@Valid @RequestBody Customer customer) {
         return service.addCustomer(customer);
     }
 
     @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Customer getCustomer(@PathVariable int customerId){
+    public Customer getCustomer(@PathVariable int customerId) {
         return service.getCustomer(customerId);
     }
 
-    @RequestMapping(value = "/customers", method =  RequestMethod.GET)
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public List<Customer> getAllCustomers(){
+    public List<Customer> getAllCustomers() {
         return service.getAllCustomers();
     }
 
     @RequestMapping(value = "/customers", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void updateCustomer(@Valid @RequestBody Customer customer){
+    public void updateCustomer(@Valid @RequestBody Customer customer) {
         service.updateCustomer(customer);
     }
 
-    @RequestMapping(value = "/customers/{customerId}", method= RequestMethod.DELETE)
+    @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCustomer(@PathVariable int customerId){
+    public void deleteCustomer(@PathVariable int customerId) {
         service.deleteCustomer(customerId);
     }
-
-
-
 
 
     //PRODUCT ENDPOINTS
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public Product addProduct(@Valid @RequestBody Product product){
+    public Product addProduct(@Valid @RequestBody Product product) {
         return service.addProduct(product);
     }
 
-    @RequestMapping(value = "/products/{productId}", method =  RequestMethod.GET)
+    @RequestMapping(value = "/products/{productId}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Product getProduct(@PathVariable int productId){
+    public Product getProduct(@PathVariable int productId) {
         return service.getProduct(productId);
     }
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return service.getAllProducts();
 
     }
@@ -77,16 +75,15 @@ public class AdminServiceController {
 
     @RequestMapping(value = "/products", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void updateProducts(@Valid @RequestBody Product product){
+    public void updateProducts(@Valid @RequestBody Product product) {
         service.updateProduct(product);
     }
 
     @RequestMapping(value = "/products/{productId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteProduct(@PathVariable int productId){
+    public void deleteProduct(@PathVariable int productId) {
         service.deleteProduct(productId);
     }
-
 
 
     //LEVEL UP ENDPOINTS
@@ -94,46 +91,45 @@ public class AdminServiceController {
 
     @RequestMapping(value = "/levelUp", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public LevelUp addLevelUp(@Valid @RequestBody LevelUp levelUp){
+    public LevelUp addLevelUp(@Valid @RequestBody LevelUp levelUp) {
         return service.addLevelUp(levelUp);
     }
 
     @RequestMapping(value = "/levelUp/{levelUpId}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public LevelUp getLevelUp(@PathVariable int levelUpId){
+    public LevelUp getLevelUp(@PathVariable int levelUpId) {
         return service.getLevelUp(levelUpId);
     }
 
     @RequestMapping(value = "/levelUp", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public List<LevelUp> getAllLevelUps(){
+    public List<LevelUp> getAllLevelUps() {
         return service.getAllLevelUps();
     }
 
     @RequestMapping(value = "/levelUp", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public void updateLevelUp(@Valid @RequestBody LevelUp levelUp){
+    public void updateLevelUp(@Valid @RequestBody LevelUp levelUp) {
         service.updateLevelUp(levelUp);
     }
 
 
     @RequestMapping(value = "/levelUp/{levelUpId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteLevelUp(@PathVariable int levelUpId){
+    public void deleteLevelUp(@PathVariable int levelUpId) {
         service.deleteLevelUp(levelUpId);
     }
 
 
-
     //INVENTORY ENDPOINTS
 
-    @RequestMapping(value = "/inventory", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
+//    @RequestMapping(value = "/inventory", method = RequestMethod.POST)
+//    @ResponseStatus(HttpStatus.CREATED)
+//
+//    public Inventory addInventory(@Valid @RequestBody Inventory inventory){
+//        service.
+//    }
+//}
 
-    public Inventory addInventory(@Valid @RequestBody Inventory inventory){
-        service.
-    }
 }
-
-
 
